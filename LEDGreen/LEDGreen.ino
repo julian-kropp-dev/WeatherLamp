0cf8c7146af4acb6e02b824b0754c6bc31b745b1
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-12-20 16:34:44 +0100

    Updated code for LED Fading Animation

--- LEDGreen/LEDGreen.ino
+++ LEDGreen/LEDGreen.ino
@@ -1,27 +1,6 @@
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 #include "FastLED.h"
-
-#define FASTLED_ALLOW_INTERRUPTS 0
 #define x 100
 
-
-
-// SCL GPIO5
-// SDA GPIO4
-#define OLED_RESET 0  // GPIO0
-Adafruit_SSD1306 display(OLED_RESET);
-
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-
-#define LOGO16_GLCD_HEIGHT 16
-#define LOGO16_GLCD_WIDTH  16
 #define DATA_PIN    D5
 //#define CLK_PIN   4
 #define LED_TYPE    WS2812B
@@ -30,9 +9,6 @@ Adafruit_SSD1306 display(OLED_RESET);
 CRGB leds[NUM_LEDS];
 
 #define BRIGHTNESS          80
-#define FRAMES_PER_SECOND  120
-
-
 
 void setup() {
   Serial.begin(9600);
