ce67b156ca1c9db30df9c13b135ce0012e5c4ccc
julian-kropp-dev-MacBook <77173321+julian-kropp-dev@users.noreply.github.com>
2021-12-23 14:16:08 +0100

    ChristmasUpdate

--- LEDGreen/LEDGreen.ino
+++ LEDGreen/LEDGreen.ino
@@ -1,5 +1,15 @@
+//OLED Display
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#define OLED_RESET 0
+#define OLED_WIDTH 64
+#define OLED_HEIGHT 48
+#define OLED_ADDR   0x3C
+Adafruit_SSD1306 display(OLED_RESET);
+//LED
 #include <FastLED.h>
-
+#define PAUSE 1000
 #define DATA_PIN D5
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
@@ -21,14 +31,31 @@ void setup()
 
   // max 400mA ziehen
   FastLED.setMaxPowerInVoltsAndMilliamps(5, 400);
+
+  //Display
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+
+  display.setTextSize(0.5);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("Wetter fur Oldenburg");
+
+  display.setTextSize(0.5);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 24);
+  display.println("Temperatur");
+  display.println("-3 Grad");
+  
+
+  display.display();
+  
 }
 
 //Wird unendlich oft abgespielt
 void loop() {
-  int y = random(0,3);
-  if (y == 0) greenfading();
-  if (y == 1) greenflashlight();
-  else singleflashlight();
+  greenfading();
+ 
 }
 
 void greenfading()
